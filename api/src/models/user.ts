30b7feb2c2aa8eff8d3b946d82ab1e0cb5310fe5
Esteban Solis <estebansolisd@gmail.com>
2024-05-24 14:45:12 -0600

    moving api to separated folder

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -4,7 +4,8 @@ import { Table, Column, Model, DataType } from 'sequelize-typescript';
   timestamps: true,
   tableName: 'users'
 })
-export class User extends Model<User> {
+
+class User extends Model {
   @Column({
     type: DataType.STRING,
     allowNull: false
@@ -23,3 +24,5 @@ export class User extends Model<User> {
   })
   password!: string;
 }
+
+export default User;
