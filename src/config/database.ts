dedfe68b05b29e8e00f7a637bd514e806b02ef8b
Esteban Solis <estebansolisd@gmail.com>
2024-05-23 20:00:44 -0600

    fixing initial commit

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const sequelize = new Sequelize({
   database: process.env.DB_NAME,
-  dialect: 'mysql' as Dialect,
+  dialect: 'postgres' as Dialect,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
